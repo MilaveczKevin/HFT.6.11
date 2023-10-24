7a14a9cf4a03e9ac5914ef7037d55966056b989e
milavecz.kevin <milaveczkevin@gmail.com>
2023-10-24 18:33:45 +0200

    Add project files.

---
+++ HFT.6.11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HFT._6._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
